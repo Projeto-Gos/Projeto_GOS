59148e43d2691fd2c42f8441f488a33e19b48358
RuanJonys <ruan.abreu4@aluno.ce.gov.br>
2024-09-02 11:33:03 -0300

    Cadastro de usuario e Login

--- usuario/cad_usuario.php
+++ usuario/cad_usuario.php
@@ -1,92 +1,128 @@
-<?php
-    include_once('config/conexao.php'); // Inclui o arquivo de conexão com o banco de dados
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GOS</title>
+    <link rel="stylesheet" href="static/css/style.css">
+    <link rel="stylesheet" href="static/css/cad_user.css">
 
-    // Verifica se o formulário foi enviado
-    if (isset($_POST['botao'])) {
-        // Recebe os dados do formulário
-        $nome = $_POST['nome'];
-        $email = $_POST['email'];
-        $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT); // Usando hash seguro para a senha
+    <!--box link icons-->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
 
-        // Verifica se foi enviado algum arquivo de foto
-        if (!empty($_FILES['foto']['name'])) {
-            $formatosPermitidos = array("png", "jpg", "jpeg"); // Formatos permitidos
-            $extensao = pathinfo($_FILES['foto']['name'], PATHINFO_EXTENSION); // Obtém a extensão do arquivo
+    <!--remix link icons-->
+    <link
+    href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet"/>
+    
 
-            // Verifica se a extensão do arquivo está nos formatos permitidos
-            if (in_array(strtolower($extensao), $formatosPermitidos)) {
-                $pasta = "img/user/"; // Define o diretório para upload
-                $temporario = $_FILES['foto']['tmp_name']; // Caminho temporário do arquivo
-                $novoNome = uniqid() . ".$extensao"; // Gera um nome único para o arquivo
+</head>
 
-                // Move o arquivo para o diretório de imagens
-                if (move_uploaded_file($temporario, $pasta . $novoNome)) {
-                    // Sucesso no upload da imagem
-                } else {
-                    echo '<div class="container">
-                            <div class="alert alert-danger alert-dismissible">
-                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                                <h5><i class="icon fas fa-exclamation-triangle"></i> Erro!</h5>
-                                Não foi possível fazer o upload do arquivo.
-                            </div>
-                        </div>';
-                    exit(); // Termina a execução do script após o erro
-                }
-            } else {
-                echo '<div class="container">
-                        <div class="alert alert-danger alert-dismissible">
-                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                            <h5><i class="icon fas fa-exclamation-triangle"></i> Formato Inválido!</h5>
-                            Formato de arquivo não permitido.
-                        </div>
-                    </div>';
-                exit(); // Termina a execução do script após o erro
-            }
-        } else {
-            // Define um avatar padrão caso não seja enviado nenhum arquivo de foto
-            $novoNome = 'avatar-padrao.png'; // Nome do arquivo de avatar padrão
-        }
+<body>
 
-        // Prepara a consulta SQL para inserção dos dados do usuário
-        $cadastro = "INSERT INTO tb_user (foto_user, nome_user, email_user, senha_user) VALUES (:foto, :nome, :email, :senha)";
-
-        try {
-            $result = $conect->prepare($cadastro);
-            $result->bindParam(':nome', $nome, PDO::PARAM_STR);
-            $result->bindParam(':email', $email, PDO::PARAM_STR);
-            $result->bindParam(':senha', $senha, PDO::PARAM_STR);
-            $result->bindParam(':foto', $novoNome, PDO::PARAM_STR);
-            $result->execute();
+    <header>
+        <nav class="navbar">
+            <a href="index.html" class="logo">
+                <img src="static/img/logo_coral.svg" alt="Logo" />
+            </a>
             
-            $contar = $result->rowCount();
+            <ul class="nav-links">
+                <li><a href="index.html">Início</a></li>
+                <li><a href="#about">Atividades</a></li>
+                <li><a href="#services">Cadastros</a></li>
+                <li><a href="#contact">Relatório</a></li>
+            </ul>
 
-            if ($contar > 0) {
-                echo '<div class="container">
-                        <div class="alert alert-success alert-dismissible">
-                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                            <h5><i class="icon fas fa-check"></i> OK!</h5>
-                            Dados inseridos com sucesso !!!
-                        </div>
-                    </div>';
-            } else {
-                echo '<div class="container">
-                        <div class="alert alert-danger alert-dismissible">
-                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                            <h5><i class="icon fas fa-check"></i> Erro!</h5>
-                            Dados não inseridos !!!
-                        </div>
-                    </div>';
-            }
-        } catch (PDOException $e) {
-            // Loga a mensagem de erro em vez de exibi-la para o usuário
-            error_log("ERRO DE PDO: " . $e->getMessage());
-            echo '<div class="container">
-                    <div class="alert alert-danger alert-dismissible">
-                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                        <h5><i class="icon fas fa-exclamation-triangle"></i> Erro!</h5>
-                        Ocorreu um erro ao tentar inserir os dados.
-                    </div>
-                </div>';
-        }
-    }
-?>
+            <div class="entrar">
+                <a href="login.html">Entrar</a> 
+            </div>
+        </nav>
+    </header>
+
+    <section>
+
+        <!--formulário-->
+        <div class="cadastro-container">
+            <form action="cadastro.php" method="post" class="cadastro-form" enctype="multipart/form-data">
+    
+                <h2>Cadastro</h2>
+    
+                <div class="form-group">
+                    <input type="file" id="foto" name="foto" accept="image/*" required>
+                    <i class='bx bxs-camera' style='color:#fffcf2'></i>
+                </div>
+    
+                <div class="form-group">
+                    <input type="text" id="nome" name="nome" placeholder="Nome" required>
+                    <i class='bx bxs-user' style='color:#fffcf2'></i>
+                </div>
+    
+                <div class="form-group">
+                    <input type="email" id="email" name="email" placeholder="Email" required>
+                    <i class='bx bxs-envelope' style='color:#fffcf2'></i>
+                </div>
+    
+                <div class="form-group">
+                    <input type="password" id="senha" name="senha" placeholder="Senha" required>
+                    <i class='bx bxs-lock-alt' style='color:#fffcf2'></i>
+                </div>
+    
+                <button type="submit" class="cadastro-btn">Cadastrar</button>
+    
+                <div class="login">
+                    <p>Já possui uma conta? <a href="login.html">Entrar</a></p>
+                </div>
+            </form>
+        </div>
+        <!--animação-->
+        <div class="animacao-cadastro">
+            <img src="static/gif/cad_user.svg" alt="Animação de Cadastro">
+        </div>
+    </section>
+    
+    <footer>
+        <div class="footer-container">
+            <div class="footer-box">
+                <h3>GOS</h3>
+                <p>É tudo uma questão de seus sonhos.</p>
+                <div class="icons-sociais">
+                    <a href="#"><i class="bx bxl-instagram"></i></a>
+                    <a href="#"><i class="bx bxl-facebook"></i></a>
+                    <a href="#"><i class="bx bxl-whatsapp"></i></a>
+                </div>
+            </div>
+            
+            <div class="footer-box">
+                <h3>Navegação</h3>
+                <ul>
+                    <li><a href="#">Início</a></li>
+                    <li><a href="#">Atividades</a></li>
+                    <li><a href="#">Cadastros</a></li>
+                    <li><a href="#">Relatório</a></li>
+                </ul>
+            </div>
+            
+            <div class="footer-box">
+                <h3>Suporte</h3>
+                <ul>
+                    <li><a href="#">FAQs</a></li>
+                    <li><a href="#">Central de Ajuda</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </div>
+            
+            <div class="footer-box">
+                <h3>Inscreva-se</h3>
+                <p>Digite seu e-mail para ser notificado sobre nossas notícias</p>
+                <div class="inscreva-form">
+                    <input type="email" placeholder="Seu email" required>
+                    <button type="submit"><i class="bx bx-envelope" style="color: #191B24; font-size: 15px;" ></i></button>
+                </div>
+            </div>
+        </div>
+        <div class="footer-btn">
+            <p>© 2024 todos os direitos reservados</p>
+        </div>
+    </footer>
+</body>
+</html>
+    
--- usuario/login.php
+++ usuario/login.php
@@ -1,80 +1,119 @@
-<?php
-    session_start(); 
-  
-    // Verifica se o usuário está autenticado (verifica se a sessão está ativa e se o usuário está logado)
-    if (isset($_SESSION['loginUser']) && $_SESSION['senhaUser'] === true) {
-        // Redireciona para a página home
-        header("Location: paginas/home.php");
-    }
-?>
-<!-- -------------------------------------------------------------------------------------------------------------------------------------- -->
-<?php
-    include_once('config/conexao.php');
-                
-    // Exibir mensagens com base na ação
-    if (isset($_GET['acao'])) {
-        $acao = $_GET['acao'];
-        if ($acao == 'negado') {
-            echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">×</button>
-            <strong>Erro ao Acessar o sistema!</strong> Efetue o login ;(</div>';
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GOS</title>
+    <link rel="stylesheet" href="static/css/style.css">
+    <link rel="stylesheet" href="static/css/login.css">
+
+    <!--box link icons-->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+    <!--remix link icons-->
+    <link
+    href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet"/>
+    
+
+</head>
+
+<body>
+
+    <header>
+        <nav class="navbar">
+            <a href="index.html" class="logo">
+                <img src="static/img/logo_coral.svg" alt="Logo" />
+            </a>
             
-        } elseif ($acao == 'sair') {
-            echo '<div class="alert alert-warning"><button type="button" class="close" data-dismiss="alert">×</button>
-            <strong>Você acabou de sair da Agenda Eletrônica!</strong> :(</div>';
+            <ul class="nav-links">
+                <li><a href="index.html">Início</a></li>
+                <li><a href="#about">Atividades</a></li>
+                <li><a href="#services">Cadastros</a></li>
+                <li><a href="#contact">Relatório</a></li>
+            </ul>
+
+            <div class="entrar">
+                <a href="login.html">Entrar</a> 
+            </div>
+        </nav>
+    </header>
+
+    <section>
+        <div class="animacao-login">
+            <img src="static/gif/login.svg" alt="Animação de Login">
+        </div>
+
+        <div class="login-container">
+            <form action="login.php" method="post" class="login-form">
+
+                <h2>Login</h2>
+
+                <div class="form-group">
+                    <input type="email" id="email" name="email" placeholder="Email">
+                    <i class='bx bxs-user' style='color:#fffcf2'></i>
+                </div>
+
+                <div class="form-group">
+                    <input type="password" id="password" name="password" placeholder="Senha">
+                    <i class='bx bxs-lock-alt' style='color:#fffcf2'></i>
+                </div>
+
+                <button type="submit" class="btn">Entrar</button>
+
+                <div class="cadastro">
+                    <p>Não possui uma conta? <a href="cad_user.html">Cadastre-se</a></p>
+                </div>
+
+            </form>
+        </div>
+    </section>
+        
+    <footer>
+        <div class="footer-container">
+            <div class="footer-box">
+                <h3>GOS</h3>
+                <p>É tudo uma questão de seus sonhos.</p>
+                <div class="icons-sociais">
+                    <a href="#"><i class="bx bxl-instagram"></i></a>
+                    <a href="#"><i class="bx bxl-facebook"></i></a>
+                    <a href="#"><i class="bx bxl-whatsapp"></i></a>
+                </div>
+            </div>
             
-        }
-      }
-      
-      // Processar o formulário de login
-      if (isset($_POST['login'])) {
-          $login = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-          $senha = filter_input(INPUT_POST, 'senha', FILTER_DEFAULT);
-      
-          if ($login && $senha) {
-              $select = "SELECT * FROM tb_user WHERE email_user = :emailLogin";
-      
-              try {
-                  $resultLogin = $conect->prepare($select);
-                  $resultLogin->bindParam(':emailLogin', $login, PDO::PARAM_STR);
-                  $resultLogin->execute();
-      
-                  $verificar = $resultLogin->rowCount();
-                  if ($verificar > 0) {
-                      $user = $resultLogin->fetch(PDO::FETCH_ASSOC);
-      
-                      // Verifica a senha
-                      if (password_verify($senha, $user['senha_user'])) {
-                          // Criar sessão
-                          $_SESSION['loginUser'] = $login;
-                          $_SESSION['senhaUser'] = $user['id_user'];
-      
-                          echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">×</button>
-                          <strong>Logado com sucesso!</strong> Você será redirecionado para a agenda :)</div>';
-      
-                          header("Refresh: 3; url=paginas/home.php?acao=bemvindo");
-                      } else {
-                          echo '<div class="alert alert-danger">
-                          <button type="button" class="close" data-dismiss="alert">×</button>
-                          <strong>Erro!</strong> Senha incorreta, tente novamente.</div>';
-                          header("Refresh: 5; url=index.php");
-                      }
-                  } else {
-                      echo '<div class="alert alert-danger">
-                      <button type="button" class="close" data-dismiss="alert">×</button>
-                      <strong>Erro!</strong> E-mail não encontrado, verifique seu login ou faça o cadastro.</div>';
-                      header("Refresh: 7; url=index.php");
-                  }
-              } catch (PDOException $e) {
-                  // Log the error instead of displaying it to the user
-                  error_log("ERRO DE LOGIN DO PDO: " . $e->getMessage());
-                  echo '<div class="alert alert-danger">
-                  <button type="button" class="close" data-dismiss="alert">×</button>
-                  <strong>Erro!</strong> Ocorreu um erro ao tentar fazer login. Por favor, tente novamente mais tarde.</div>';
-              }
-          } else {
-              echo '<div class="alert alert-danger">
-              <button type="button" class="close" data-dismiss="alert">×</button>
-              <strong>Erro!</strong> Todos os campos são obrigatórios.</div>';
-          }
-      }
-?>
+            <div class="footer-box">
+                <h3>Navegação</h3>
+                <ul>
+                    <li><a href="#">Início</a></li>
+                    <li><a href="#">Atividades</a></li>
+                    <li><a href="#">Cadastros</a></li>
+                    <li><a href="#">Relatório</a></li>
+                </ul>
+            </div>
+            
+            <div class="footer-box">
+                <h3>Suporte</h3>
+                <ul>
+                    <li><a href="#">FAQs</a></li>
+                    <li><a href="#">Central de Ajuda</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </div>
+            
+            <div class="footer-box">
+                <h3>Inscreva-se</h3>
+                <p>Digite seu e-mail para ser notificado sobre nossas notícias</p>
+                <div class="inscreva-form">
+                    <input type="email" placeholder="Seu email" required>
+                    <button type="submit"><i class="bx bx-envelope" style="color: #191B24; font-size: 15px;" ></i></button>
+                </div>
+            </div>
+        </div>
+        <div class="footer-btn">
+            <p>© 2024 todos os direitos reservados</p>
+        </div>
+    </footer>
+
+
+    <script src="static/js/script.js"></script>
+</body>
+</html>
